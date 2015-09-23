f36ce918c63bdcc510ee8be644f339fc9904f98d
System Administrator <malcolm.gin@gmail.com>
2015-09-23 10:17:58 -0700

    Added turnRight method and appropriate unit tests.

--- rover.py
+++ rover.py
@@ -3,11 +3,37 @@
 class Rover:
   """Rover class to fulfill Rover Kata.
   Tracks positionX, position Y, facing, and possibly other attributes.
-  Includes many methods for moving and interacting with the WorldGrid class."""
+  Includes many methods for moving and interacting with the WorldGrid class.
+
+  Assumptions:
+    positionX = 0 && positionY = 0 are default origin starting coordinates.
+    Grid for position is an arbitrarily large 2 dimensional grid of positive and negative integers.
+    Valid facing is one element of ["North", "South", "East", "West"]
+    Default start position is (0, 0) and North facing if not stated otherwise."""
   positionX = 0
   positionY = 0
   facing = "North"
 
+  def startRover(self, startingCoords, startingFace):
+    """Rover given a startRover() command is moved to specific startingCoords tuple (positionX, positionY) with specified
+    startingFace facing. No validation of incoming values here."""
+    self.positionX = startingCoords[0]
+    self.positionY = startingCoords[1]
+    self.facing = startingFace
+
+  def turnRight(self):
+    """turnRight() method spins rover clockwise (as seen from above) 90 degrees on each command. Basic validation of
+    self.facing values is done here, with an unknown value raising an exception. Probably needs refactoring."""
+    if (self.facing == "North"):
+      self.facing = "East"
+    elif (self.facing == "East"):
+      self.facing = "South"
+    elif (self.facing == "South"):
+      self.facing = "West"
+    elif (self.facing == "West"):
+      self.facing = "North"
+    else:
+      raise ValueError('Facing value not in ["North", "South", "East", "West"]')
   
 
 class WorldGrid:
--- testClass.py
+++ testClass.py
@@ -1,4 +1,4 @@
-from nose.tools import assert_is, assert_is_instance, assert_in, assert_equal
+from nose.tools import assert_is, assert_is_instance, assert_in, assert_equal, raises
 from rover import *
 
 class TestRover:
@@ -12,3 +12,29 @@ class TestRover:
     assert_equal(0, rv.positionX)
     assert_equal(0, rv.positionY)
     assert_equal("North", rv.facing)
+
+  def testRoverSetup(self):
+    rv = Rover()
+    rv.startRover((10, 5), "South")
+    assert_equal(10, rv.positionX)
+    assert_equal(5, rv.positionY)
+    assert_equal("South", rv.facing)
+
+  def testRoverTurnRight(self):
+    rv = Rover()
+    assert_equal("North", rv.facing)
+    rv.turnRight()
+    assert_equal("East", rv.facing)
+    rv.turnRight()
+    assert_equal("South", rv.facing)
+    rv.turnRight()
+    assert_equal("West", rv.facing)
+    rv.turnRight()
+    assert_equal("North", rv.facing)
+
+  @raises(ValueError)
+  def testRoverTurnRightException(self):
+    rv = Rover()
+    rv.startRover((10, 5), "Elephant")
+    rv.turnRight()
+    
