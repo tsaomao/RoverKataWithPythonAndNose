7f6034c5d7bfbc00873fc5c0616da79f3b0b0722
System Administrator <malcolm.gin@gmail.com>
2015-09-23 09:51:52 -0700

    Added Python docstrings to working module, rover.py

--- rover.py
+++ rover.py
@@ -1,5 +1,17 @@
+"""Module to implement Rover Kata. See README.md for more information."""
+
 class Rover:
+  """Rover class to fulfill Rover Kata.
+  Tracks positionX, position Y, facing, and possibly other attributes.
+  Includes many methods for moving and interacting with the WorldGrid class."""
   positionX = 0
   positionY = 0
   facing = "North"
+
+  
+
+class WorldGrid:
+  """WorldGrid class will return information about the world including whether there exists
+  an obstacle at a specified tuple. WorldGrid is 2 dimensional, but may be refactored to other dimensions and/or
+  general properties and behavior."""
 
